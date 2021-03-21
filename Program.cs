6affb37936b0acf90b7272d05597e844edd1e892
RD-Ricardo <ricardodias169@gmail.com>
2021-03-20 22:20:46 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("-------------------");
+            Console.WriteLine("--Numero Extensao--");
+            Console.WriteLine("-------------------");
+            Numero  = Convert.ToInt32(Console.ReadLine());
+            string numberText = number.ToWords();
+            Console.Write(numberText);
+
+        }
+    }
+}
